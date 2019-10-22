4b07497244d79f60a659cf4ecf5b986d3f544d7f
ronghuayao <ronghua.yao@ccccri.com>
2019-10-22 18:10:30 +0800

    update scrapy

--- python/spider/eastmoney/eastmoney/mymiddlewares.py
+++ python/spider/eastmoney/eastmoney/mymiddlewares.py
@@ -10,8 +10,9 @@ from scrapy.http import HtmlResponse
 from scrapy.selector import Selector
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from logging import getLogger
 
 
@@ -20,13 +21,20 @@ class SeleniumMiddleware(object):
         if spider.name == 'get_stock' and request.url == 'http://quote.eastmoney.com/center/gridlist.html#hs_a_board':
             driver = webdriver.Firefox()
             driver.get(request.url)
-            WebDriverWait(driver, 100).until(
-                EC.presence_of_element_located((By.ID, "table_wrapper-table"))
-            )
-            html = driver.page_source
-            se = Selector(text=html)
-            next_page = se.xpath('//*[@id="main-table_paginate"]/a[2]')
-            driver.quit()
+            next_page = True
+            while next_page:
+                WebDriverWait(driver, 100).until(
+                    EC.presence_of_element_located((By.ID, "table_wrapper-table"))
+                )
+                try:
+                    next_page_btn = driver.find_element_by_xpath(
+                        '//*[@id="main-table_paginate"]/a[@class="next paginate_button"]')
+                    next_page_btn.click()
+                except NoSuchElementException:
+                    next_page = False
+                finally:
+                    html = driver.page_source
+                    # driver.quit()
             return HtmlResponse(url=request.url, body=html, request=request, encoding='utf-8')
 
     # def spider_opened(self, spider):
