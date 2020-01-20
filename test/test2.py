d0322943ec65e0e89e7089d416da8d86c034b598
ronghuayao <ronghua.yao@ccccri.com>
2020-01-20 18:03:43 +0800

    update note

--- test/test2.py
+++ test/test2.py
@@ -1,20 +1,20 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        ans = {')': '(', ']': '[', '}': '{'}
-        s = list(s)
-        stack = []
-        while s:
-            ele = s.pop(0)
-            if stack and stack[-1] == ans.get(ele):
-                stack.pop()
+    def evalRPN(self, tokens) -> int:
+        stack=[]
+        while tokens:
+            ele = tokens.pop(0)
+            if ele in '+-*/':
+                # 出栈两个，算完入栈
+                n2 = stack.pop()
+                n1 = stack.pop()
+                n3 = int(eval(n1+ele+n2))
+                stack.append(str(n3))
             else:
                 stack.append(ele)
-        if stack:
-            print(False)
-            return False
-        print(True)
-        return True
+        print(int(stack[0]))
+        return stack[0]
 
-w = "([)]"
+
+
 s=Solution()
-s.isValid(w)
+s.evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
