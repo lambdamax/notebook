0807838a3d042d406e3ae37f86ded346803e74ef
yrh <370450848@qq.com>
2020-03-02 23:59:46 +0800

    update note

--- test/test2.py
+++ test/test2.py
@@ -1,20 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 class Solution:
-    def evalRPN(self, tokens) -> int:
-        stack=[]
-        while tokens:
-            ele = tokens.pop(0)
-            if ele in '+-*/':
-                # 出栈两个，算完入栈
-                n2 = stack.pop()
-                n1 = stack.pop()
-                n3 = int(eval(n1+ele+n2))
-                stack.append(str(n3))
-            else:
-                stack.append(ele)
-        print(int(stack[0]))
-        return stack[0]
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head.next:
+            return head
+        new_node = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_node
 
 
-
-s=Solution()
-s.evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"])
+l5 = ListNode(5)
+l4 = ListNode(4)
+l4.next = l5
+l3 = ListNode(3)
+l3.next = l4
+l2 = ListNode(2)
+l2.next = l3
+l1 = ListNode(1)
+l1.next = l2
+# print(l1)
+sl = Solution()
+r = sl.reverseList(l1)
