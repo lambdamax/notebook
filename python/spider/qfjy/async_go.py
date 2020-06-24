baef6c656c349005cf7de31a6ea33005dae47f56
YAORH <max.yao@essilorchina.com>
2020-06-24 17:31:32 +0800

    update selenium

---
+++ python/spider/qfjy/async_go.py
@@ -0,0 +1,106 @@
+from pyquery import PyQuery as pq
+import requests
+import os
+from sys import stdout
+import queue
+import asyncio
+import time
+import aiohttp
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def cost(func):
+    def wrapper():
+        now_time = time.time()
+        func()
+        print(time.time() - now_time)
+
+    return wrapper
+
+
+class QFJY:
+    def __init__(self):
+        self.url = "http://video.mobiletrain.org/course/index/courseId/548"
+        self.path = None
+        self.q = queue.Queue()
+        self.check_save_path()
+        self.workers = 10
+
+    def info(self, filename, filetype, size, url, path):
+        _logger.warning('-' * 30)
+        msg = ['文件名称:{filename}',
+               '文件类型:{filetype}',
+               '文件大小:{size}bytes',
+               '下载地址:{url}',
+               '保存路径:{path}']
+        _logger.warning("\n".join(msg).format(filename=filename, filetype=filetype, size=size, url=url, path=path))
+        _logger.warning('-' * 30)
+
+    def check_save_path(self):
+        base_path = "C:\Afiles"
+        dirname = "go"
+        self.path = os.path.join(base_path, dirname)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def parse(self):
+        req = requests.get(self.url)
+        doc = pq(req.text)
+        items = doc("div.course-list").items()
+        for item in items:
+            for a in item("a.fr").items():
+                url = a.attr("data-url")
+                self.q.put(url)
+
+    async def download(self, url, filename):
+        file_to_save = os.path.join(self.path, filename)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                filesize = r.headers["Content-Length"]
+                total_size = int(filesize)
+                self.info(filename, r.headers["Content-Type"], filesize, url, file_to_save)
+                if os.path.exists(file_to_save) and os.path.getsize(file_to_save) == total_size:
+                    _logger.warning(filename + "检测到已存在")
+                    return
+                chunk_size = 128
+                with open(file_to_save, 'wb') as f:
+                    async for content in r.content.iter_chunked(chunk_size):
+                        f.write(content)
+
+    @cost
+    async def run(self):
+        self.parse()
+        urls = []
+        while not self.q.empty():
+            if len(urls) < self.workers:
+                url = self.q.get()
+                urls.append(url)
+                if not self.q.empty():
+                    continue
+            tasks = [asyncio.create_task(self.download(url, url.split('/')[-1])) for url in urls]
+            await asyncio.gather(*tasks)
+            urls.clear()
+
+    async def test1(self):
+        await asyncio.sleep(2)
+        print("test1")
+
+    async def test2(self):
+        await asyncio.sleep(2)
+        print("test2")
+
+    async def test_run(self):
+        task1 = asyncio.create_task(self.test1())
+        task2 = asyncio.create_task(self.test2())
+        await asyncio.gather(task1, task2)
+
+        # await self.test1()
+        # await self.test2()
+
+
+if __name__ == "__main__":
+    s = QFJY()
+    # asyncio.run(s.test_run())
+    asyncio.run(s.run())
--- python/spider/qfjy/request_go.py
+++ python/spider/qfjy/request_go.py
@@ -2,23 +2,28 @@ from pyquery import PyQuery as pq
 import requests
 import os
 from sys import stdout
-import queue
-import asyncio
 import time
+
+
+def cost(func):
+    def wrapper():
+        now_time = time.time()
+        func()
+        print(time.time() - now_time)
+
+    return wrapper
 
 
 class QFJY:
     def __init__(self):
         self.url = "http://video.mobiletrain.org/course/index/courseId/548"
         self.path = None
-        self.q = queue.Queue()
         self.urls = []
         self.check_save_path()
-        self.workers = 0
 
     def check_save_path(self):
-        base_path = "D:\download"
-        dirname = "go2"
+        base_path = "C:\Afiles"
+        dirname = "go"
         self.path = os.path.join(base_path, dirname)
         if not os.path.exists(self.path):
             os.mkdir(self.path)
@@ -31,7 +36,6 @@ class QFJY:
             for a in item("a.fr").items():
                 url = a.attr("data-url")
                 self.urls.append(url)
-                self.q.put(url)
 
     def download(self, url, filename):
         file_to_save = os.path.join(self.path, filename)
@@ -48,7 +52,6 @@ class QFJY:
             print('-' * 30)
             if os.path.exists(file_to_save) and os.path.getsize(file_to_save) == total_size:
                 print("检测到已存在")
-                self.workers -= 1
                 return
             print("开始下载")
             chunk_size = 128
@@ -56,59 +59,20 @@ class QFJY:
             with open(file_to_save, 'wb') as f:
                 for chunk in r.iter_content(chunk_size):
                     f.write(chunk)
-                    # if now_size <= total_size:
-                    #     stdout.write("下载进度: %.2f \r" % (now_size / total_size * 100))
-                    #     now_size += chunk_size
-                    # else:
-                    #     stdout.write("结束下载")
-                    #     print("下载完成")
-        self.workers -= 1
-
-    async def coroutine_download(self, url, filename):
-        self.download(url, filename)
-
+                    if now_size <= total_size:
+                        stdout.write("下载进度: %.2f \r" % (now_size / total_size * 100))
+                        now_size += chunk_size
+                    else:
+                        stdout.write("结束下载")
+                        print("下载完成")
+    @cost
     def run(self):
         self.parse()
         for url in self.urls:
             filename = url.split('/')[-1]
             self.download(url, filename)
 
-    async def run2(self):
-        self.parse()
-        while not self.q.empty():
-            # if self.workers > 5:
-            #     await asyncio.sleep(2)
-            #     continue
-            # url = self.q.get()
-            # filename = url.split('/')[-1]
-            self.workers += 1
-            url1 = self.q.get()
-            filename1 = url1.split('/')[-1]
-            task1 = self.coroutine_download(url1, filename1)
-            url2 = self.q.get()
-            filename2 = url2.split('/')[-1]
-            task2 = self.coroutine_download(url2, filename2)
-            url3 = self.q.get()
-            filename3 = url3.split('/')[-1]
-            task3 = self.coroutine_download(url3, filename3)
-            url4 = self.q.get()
-            filename4 = url4.split('/')[-1]
-            task4 = self.coroutine_download(url4, filename4)
-            url5 = self.q.get()
-            filename5 = url5.split('/')[-1]
-            task5 = self.coroutine_download(url5, filename5)
-            await asyncio.gather(
-                task1,
-                task2,
-                task3,
-                task4,
-                task5
-            )
-
 
 if __name__ == "__main__":
     s = QFJY()
-    now = time.time()
-    # s.run()
-    asyncio.run(s.run2())
-    print(time.time() - now)
+    s.run()
