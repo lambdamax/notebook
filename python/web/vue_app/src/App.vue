4ba8f014ce2ce6e3596501cb7e58c974ff03df44
yrh <370450848@qq.com>
2020-08-16 23:08:26 +0800

    update vue frame

---
+++ python/web/vue_app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    hello vue
+    <input type="text" ref="mytext">
+    <button @click="handleAdd">add</button>
+
+    <ul>
+      <li v-for="data in datalist" :key="data">
+        {{ data }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      datalist: []
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.datalist.push(this.$refs.mytext.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+ul {
+  li {
+    background: yellow
+  }
+}
+</style>
